c48ced29936a4d8bef36c9954b09f8cc7ab7beb9
Shaziya Shaikh <shaikhshaziyashaziya6@gmail.com>
2025-10-27 20:07:37 +0530

    Fix: use correct PostgreSQL backend for Azure

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'hospitalmanagement.wsgi.application'
 # ---------------------------------------------------------------------
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'hospitalsystem-database'),
         'USER': os.environ.get('DB_USER', 'uurzzjcygo'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'saaavMWOXUh$eMw6'),
